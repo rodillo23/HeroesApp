c3e0a2cd75f5fc801acb366c5c5a32b3d74beb4f
Rodolfo Figueroa <ficr23@hotmail.com>
2023-03-07 18:08:09 -0600

    se agrego useMemo en heroPage y HeroList

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../helpers'
 
@@ -6,7 +6,7 @@ export const HeroPage = () => {
   const navigate = useNavigate()
   const {id} = useParams()
 
-  const heroe = getHeroById(id)
+  const heroe = useMemo( () => getHeroById(id), [id])
   console.log(heroe);
 
   const onNavigateBack = () => {
@@ -39,3 +39,6 @@ export const HeroPage = () => {
     </div>
   )
 }
+
+
+//Nota: useMemo para valores y useCallback para memorizar funciones
