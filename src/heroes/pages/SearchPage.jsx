932c08ef48757e6f09a43adc6adf977677b6218c
Rodolfo Figueroa <ficr23@hotmail.com>
2023-03-09 23:06:21 -0600

    se trabajo con la pagina de busqueda, se agrego la libreria query-string para obtener los query parameters, busqueda terminada

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,75 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../components/HeroCard'
+import { getHeroesByName } from '../helpers'
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate()
+  const location = useLocation()
+  const {q = ''} = queryString.parse(location.search)
+
+  const heroes = getHeroesByName(q)
+
+  const {searchText, onInputChange} = useForm({
+    searchText: q
+  })
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault()
+    if(searchText.trim().length <= 2) return
+
+    navigate(`?q=${searchText}`)
+
+    
+  }
+
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1 className='mt-5'>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form onSubmit={ onSearchSubmit }>
+            <input 
+              type="text" 
+              placeholder='Search a Hero' 
+              className='form-control'
+              name='searchText'
+              autoComplete='off' 
+              value={searchText}
+              onChange={onInputChange} 
+            />
+            <button className='btn btn-outline-primary mt-2'>Search</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            No hero with <b>{q}</b>
+          </div>
+
+          {
+            heroes.map(heroe => <HeroCard key={heroe.id} {...heroe}/>)
+          }
+          {/* <HeroCard/> */}
+
+        </div>
+      </div>
+      
+    </>
   )
 }
